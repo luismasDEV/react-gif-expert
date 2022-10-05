f5745706cd0349cb00cba4783628763219c06604
Luis Rene Mas Mas <01luisrene@gmail.com>
2022-10-04 22:18:36 -0500

    Primer commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,27 @@
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GitItem } from './GitItem';
+
+export const GifGrid = ({category}) => {
+
+    const {images, isLoading} = useFetchGifs( category );
+
+  return (
+    <>
+        <h3>{category}</h3>
+        {
+          isLoading && (<h2>Cargando...</h2>)
+        }
+        
+        <div className='card-grid'>
+          {
+            images.map( (image) => (
+              <GitItem 
+                key={ image.id } 
+                {... image}
+              />
+            ))
+          }
+        </div>
+    </>
+  )
+}
